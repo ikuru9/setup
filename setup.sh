4b49237ca5260a125bfb4273a86e664f83c0f4c1
ikuru9 <ikuru9@gmail.com>
2026-01-06 13:30:31 +0900

    Ghostty

--- setup.sh
+++ setup.sh
@@ -9,18 +9,25 @@ cp ./configs/gitconfig ~/.gitconfig
 source ~/.gitconfig
 
 # Install Oh My Zsh
-# brew install zsh powerlevel10k zsh-autosuggestions zsh-syntax-highlighting
-# cp ./configs/zshrc ~/.zshrc
-# cp ./configs/p10k.zsh ~/.p10k.zsh
-# cp ./configs/zprofile ~/.zprofile
-# source ~/.zprofile
+brew install zsh powerlevel10k zsh-autosuggestions zsh-syntax-highlighting
+cp ./configs/zshrc ~/.zshrc
+cp ./configs/p10k.zsh ~/.p10k.zsh
+cp ./configs/zprofile ~/.zprofile
+source ~/.zprofile
 
 # Install Utilities
 brew install --cask jordanbaird-ice appcleaner raycast rectangle-pro mos
 
 # Install Developer Utilities
 brew install fnm
-brew install --cask font-fira-code-nerd-font zed visual-studio-code warp
+brew install --cask font-fira-code-nerd-font zed visual-studio-code ghostty
+
+# Install Terminal Utility
+brew install --cask ghostty
+git clone https://github.com/catppuccin/ghostty.git
+mkdir -p ~/.config/ghostty/themes
+cp ./ghostty/themes ~/.config/ghostty/themes/
+cp ./config/ghostty.conf ~/.config/ghostty/config
 
 # Install Office Utilities
 brew install --cask libreoffice libreoffice-language-pack
